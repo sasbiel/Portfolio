2f6f1d52bcc59013371c02134a6a375470cfb99e
sasbiel <ssausangrt@gmail.com>
2025-11-26 15:09:06 +0700

    first commit

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Navbar from '../components/Navbar'
-import Home from '../pages/home'
+import Home from '../pages/Home'
 import Project from '../pages/Project'
 
 const Router = () => {
