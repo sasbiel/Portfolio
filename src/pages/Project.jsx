08f28c4d00d1f9a9ac8ab11566536b5922dd1040
sasbiel <ssausangrt@gmail.com>
2025-11-26 14:57:12 +0700

    first commit

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -3,10 +3,10 @@ import Navbar from '../components/Navbar'
 import cacah from '../../public/images/cacah.png'
 import figmaLogo from '../../public/images/figma-logo.png'
 import rasio from '../../public/images/rasio.png'
-import data from '../data/data'
+import dataList from '../data/data'
 
 const Project = () => { 
-  const [data, setData] = useState(data)
+  const [data, setData] = useState(dataList)
   return (
     <div className='flex flex-col w-full h-full bg-[radial-gradient(circle,#FFFFFF,#FEBEDD)] items-center'>
       <div className='flex flex-col w-280 items-center pt-30'>
